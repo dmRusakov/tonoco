b2008f7d39083a4e16ed802e365bda7b59df7b70
dmitriirusakov <dm.rusakov@gmail.com>
2023-08-27 17:53:58 -0400

    init

--- internal/config/entity.go
+++ internal/config/entity.go
@@ -1,5 +1,6 @@
 package config
 
 type Config struct {
-	IsProd bool `env:"IS_PROD"  env-default:"true"`
+	Name   string `env:"ADMIN_NAME" env-default:"MonkeysMoonAdmin"`
+	IsProd bool   `env:"ADMIN_IS_PROD" env-default:"true"`
 }
