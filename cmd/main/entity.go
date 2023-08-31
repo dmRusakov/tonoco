80446149a8d4c31f88c46ea663476e26e87bf30d
dmitriirusakov <dm.rusakov@gmail.com>
2023-08-31 19:56:29 -0400

    app init

--- cmd/main/entity.go
+++ cmd/main/entity.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/dmRusakov/monkeysmoon-admin/internal/config"
+import (
+	"github.com/dmRusakov/monkeysmoon-admin/internal/config"
+	"github.com/dmRusakov/monkeysmoon-admin/pkg/logrus"
+)
 
 type App struct {
-	cfg *config.Config
+	cfg    *config.Config
+	logger *logrus.Logrus
 }
 
 var app App = App{}
--- cmd/main/init.go
+++ cmd/main/init.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/dmRusakov/monkeysmoon-admin/internal/config"
+import (
+	"github.com/dmRusakov/monkeysmoon-admin/internal/config"
+	"github.com/dmRusakov/monkeysmoon-admin/pkg/logrus"
+)
 
 func init() {
-	// get config
+	// get config (read ENV variables)
 	app.cfg = config.GetConfig()
+
+	// make logger (logrus)
+	logrus.Init()
+	logger := logrus.GetLogrus()
+	app.logger = &logger
+	app.logger.Info("logger initialized")
+
 }
