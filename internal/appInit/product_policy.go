88a09102073515a4412c6d3ac32890d2b89e3882
Dmitrii Rusakov <78332305+dmRusakov@users.noreply.github.com>
2024-02-04 21:54:42 -0500

    Init GRPC

--- internal/appInit/product_policy.go
+++ internal/appInit/product_policy.go
@@ -1,17 +1,15 @@
 package appInit
 
 import (
-	"fmt"
 	productPolicy "github.com/dmRusakov/tonoco/internal/domain/policy/product"
-	"github.com/dmRusakov/tonoco/internal/domain/product/dao"
+	"github.com/dmRusakov/tonoco/internal/domain/product/model"
 	"github.com/dmRusakov/tonoco/internal/domain/product/service"
 )
 
 func (a *App) ProductPolicyInit() (err error) {
-	productStorage := dao.NewProductStorage(a.sqlDB)
+	productStorage := model.NewProductStorage(a.sqlDB)
 	productService := service.NewProductService(productStorage)
 	a.ProductPolicy = productPolicy.NewProductPolicy(productService, a.generator, a.clock)
 
-	fmt.Println("2")
 	return nil
 }
