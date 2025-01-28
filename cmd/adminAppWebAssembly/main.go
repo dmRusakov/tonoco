817167c9fd177244601cd37626b609ceced5a0d0
dm <dm.rusakov@gmail.com>
2025-01-27 22:22:54 -0500

    update

--- cmd/adminAppWebAssembly/main.go
+++ cmd/adminAppWebAssembly/main.go
@@ -8,21 +8,7 @@ import (
 	"syscall/js"
 )
 
-// This calls a JS function from Go.
-func main() {}
-
-//export status
-func status() int {
-	return returnData("WebAssembly Ready.", nil)
-}
-
-//export getService
-func getService() *map[string]string {
-	person := map[string]string{"Service": "Alice!", "Aria": "30", "Serviced": "Go"}
-
-	return &person
-}
-
+// This function returns a UUID and stores the data in the session storage.
 func returnData(data any, err error) int {
 	if err != nil {
 		js.Global().Get("sessionStorage").Call("setItem", 0, err.Error())
@@ -38,4 +24,19 @@ func returnData(data any, err error) int {
 
 	js.Global().Get("sessionStorage").Call("setItem", uuid, data)
 	return uuid
+}
+
+// This calls a JS function from Go.
+func main() {}
+
+//export status
+func status() int {
+	return returnData("WebAssembly Ready.", nil)
+}
+
+//export getService
+func getService() *map[string]string {
+	person := map[string]string{"Service": "Alice!", "Aria": "30", "Serviced": "Go"}
+
+	return &person
 }
