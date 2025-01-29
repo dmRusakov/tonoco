aac4b1e931db43b5558ac1a2ff3aba634958a6ae
dm <dm.rusakov@gmail.com>
2025-01-28 20:25:27 -0500

    update

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,24 +3,26 @@ class User {
         self.user = this
     }
 
-    init() {
-        this.location = async () => {
-            return {
-                userAgent: navigator.userAgent,
-                platform: navigator.platform,
-                isSupportWebp: (() => {
-                    const elem = document.createElement('canvas');
-                    if (!!(elem.getContext && elem.getContext('2d'))) {
-                        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
-                    }
-                    return false;
-                })(),
-            }
+    async init() {
+        self.user.browser = await this.getBrowserInfo()
+    }
+
+    async getBrowserInfo() {
+        return {
+            userAgent: navigator.userAgent,
+            platform: navigator.platform,
+            isSupportWebp: (() => {
+                const elem = document.createElement('canvas');
+                if (!!(elem.getContext && elem.getContext('2d'))) {
+                    return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+                }
+                return false;
+            })(),
         }
     }
 }
 
 new User()
 document.addEventListener('DOMContentLoaded', async () => {
-    self.user.init();
+    await self.user.init();
 }, false);
